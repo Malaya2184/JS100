106204879e51b1a416074a9a84e90e282493884a
Malaya2184 <malayakumarswain8@gmail.com>
2021-04-21 00:37:15 +0530

    alan api key added and basic intent and reply tested to the app

--- conversational_news_app/src/App.js
+++ conversational_news_app/src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
 
+const alanKey = '7182b4f4feedfdadb0673bf3c40169432e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
+
+    useEffect(() => {
+        alanBtn({
+            key : alanKey,
+            onCommand : ({command})=>{
+                if (command === 'testCommand' ){
+                    alert('This code was executed');
+                }
+            }
+        })
+    }, [])
+
     return(
     <div>
         <h1>Alan Ai news Application</h1>
     </div>
-    )
+    );
 }
 
 export default App;
