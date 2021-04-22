d3cc0ca236cc01770c804f14a29bcbf1b55ea9e1
Malaya2184 <malayakumarswain8@gmail.com>
2021-04-22 17:19:58 +0530

    card component added and material ui used

--- conversational_news_app/src/App.js
+++ conversational_news_app/src/App.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
-
+import NewsCards from './components/NewsCards/NewsCards'
 const alanKey = '7182b4f4feedfdadb0673bf3c40169432e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
+    const [NewsArticles, setNewsArticles] = useState([]);
 
     useEffect(() => {
         alanBtn({
             key : alanKey,
             onCommand : ({command, articles})=>{
                 if (command === 'newHeadlines' ){
+                    // console.log(articles.length);
                     console.log(articles);
+                    // console.log(articles.length);
+                    setNewsArticles(articles)
                 }
             }
         })
@@ -19,6 +23,7 @@ const App = () =>{
     return(
     <div>
         <h1>Alan Ai news Application</h1>
+        <NewsCards articles ={NewsArticles}/>
     </div>
     );
 }
---
+++ conversational_news_app/src/components/NewsCard/NewsCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+
+const NewsCard= ({article:{ description, publishedAt, source, title, url, urlToImage},i})=> {
+    return (
+        <Card>
+            <CardActionArea>
+                <CardMedia image={urlToImage || 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F742%2F478%2Fpng-transparent-express-news-pakistan-daily-express-92-news-others-miscellaneous-television-text-thumbnail.png&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dsamaa%2BTv&tbnid=BAHDw5lb-OOneM&vet=12ahUKEwjv7rSv4ZHwAhV0geYKHXkVBK0QMygWegUIARDkAQ..i&docid=VAgJi532DI7N-M&w=360&h=180&q=news%20png%20thumbnail&ved=2ahUKEwjv7rSv4ZHwAhV0geYKHXkVBK0QMygWegUIARDkAQ'}/>
+                <div>
+                <Typography varient = "body2" color="textSecondary" component= "h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography varient = "body2" color="textSecondary" component= "h2">{source.name}</Typography>
+                </div>
+                <Typography gutterBottom varient = "h5">{title}</Typography>
+                <CardContent>
+                    <Typography varient="body2" color="textSecondary" component="p"></Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size = "small" color="primary">
+                    <Typography variant= "h5" color="textSecondary"></Typography>
+                </Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default NewsCard;
